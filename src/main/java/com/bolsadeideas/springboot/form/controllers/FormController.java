aa00cbab0fb2803ad5c797e72178f38ac8ac7d0f
Enol <grimgaiden@gmail.com>
2020-07-03 18:56:55 +0200

    Added POJO Usuario.java to include user values and validation to fields in backend

--- src/main/java/com/bolsadeideas/springboot/form/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/controllers/FormController.java
@@ -1,10 +1,17 @@
 package com.bolsadeideas.springboot.form.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import com.bolsadeideas.springboot.form.models.domain.Usuario;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -18,15 +25,20 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String porcesar(Model model,
-            @RequestParam String username,
-            @RequestParam String password,
-            @RequestParam String email) {
-        
+    public String porcesar(@Valid Usuario usuario, BindingResult result, Model model) {
+
         model.addAttribute("titulo", "Resultado form");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+
+        if(result.hasErrors()) {
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err ->{
+                errores.put(err.getField(), "El campo ".concat(err.getField().concat(err.getDefaultMessage())))
+            });
+            model.addAttribute("error", errores);
+            return "/form";
+        }
+
+        model.addAttribute("usuario", usuario);
         return "resultado";
     }
 }
