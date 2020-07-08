b5f2161fe6416d28da4ee4792923cdaca2b34953
Enol <grimgaiden@gmail.com>
2020-07-08 22:09:42 +0200

    Fixed bug sending form -- checking for method 'post' in postHandle was missing and was asking for fields not assigned and therefore null--

--- src/main/java/com/bolsadeideas/springboot/form/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolsadeideas/springboot/form/interceptors/TiempoTranscurridoInterceptor.java
@@ -19,6 +19,9 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
+        if(request.getMethod().equalsIgnoreCase("post")) {
+            return;
+        }
         long tiempoFin = System.currentTimeMillis();
         long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
         long tiempoTranscurrido = tiempoFin - tiempoInicio;
